fd085b6ab4ff14ae5c37ccae203a02fda3bc5bd4
root <root@Stphane>
2023-11-24 15:41:27 +0100

    delete test_predimage_predtexte.py

--- test_ci/run_tests.sh
+++ test_ci/run_tests.sh
@@ -18,5 +18,5 @@ python test_authen.py
 python test_autho.py
 python test_create_user.py
 python test_delete_user.py
-python test_predimage.py
-python test_predtexte.py
+#python test_predimage.py
+#python test_predtexte.py
